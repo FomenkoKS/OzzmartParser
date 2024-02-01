61f0808eaf7c7174eba6ea8caf33ff6ba33bd48a
Fomenko-KS <rik.pie@gmail.com>
2024-02-01 13:12:59 -0500

    add documentation

--- main.py
+++ main.py
@@ -32,88 +32,6 @@ def get_platform(url):
                 return platform
     return None
 
-
-f = open('locators.json')
-locators_mapping = json.load(f)
-
-app = FastAPI()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.post("/whois")
-def whois(url: str):
-    markets = ["amazon", "alibaba"]
-    domain = get_domain(url)
-
-    if domain in markets:
-        platform = domain
-    else:
-        platform = get_platform(url)
-
-    return {"domain": domain, "platform": platform}
-
-
-@app.get("/parse/{marketplace}/")
-def parse_marketplace(
-    marketplace: str,
-    me: Union[str, None] = None,
-    marketplaceID: Union[str, None] = None,
-):
-    if marketplace in locators_mapping:
-        market_locators = locators_mapping[marketplace]
-
-        with sync_playwright() as playwright:
-            chromium = playwright.chromium
-            browser = chromium.launch()
-            page = browser.new_page()
-            merchant_url = market_locators["url"].format(
-                me=me, marketplaceID=marketplaceID
-            )
-            page.goto(merchant_url)
-            next_button_disabled = False
-            data = []
-
-            while next_button_disabled == False:
-                page.wait_for_selector(market_locators["pagination-container"])
-                next_button = page.locator(
-                    "%s %s"
-                    % (
-                        market_locators["pagination-container"],
-                        market_locators["pagination-next"],
-                    )
-                )
-                data = parse_product_page(page, data, market_locators)
-                print("Collect %s products" % len(data))
-                next_button_disabled = (market_locators["pagination-disabled"]) in str(
-                    next_button.get_attribute("class")
-                )
-
-                if (
-                    next_button.element_handles()[0]
-                    .get_property("disabled")
-                    .json_value()
-                    == True
-                ):
-                    next_button_disabled = True
-
-                if next_button_disabled == False:
-                    next_button.click()
-
-            browser.close()
-            return data
-    else:
-        return {"error": "not found marketplace"}
-
-
 def parse_product_page(page, data, market_locators):
     products = page.locator(market_locators["product-card"]).element_handles()
     print("Find %s products" % len(products))
@@ -142,9 +60,38 @@ def parse_product_page(page, data, market_locators):
     return data
 
 
-@app.post("/parse")
+f = open('locators.json')
+locators_mapping = json.load(f)
+
+tags_metadata = [
+    {
+        "name": "whois",
+        "description": "Geting domain and platform information by url",
+    },
+    {
+        "name": "parse",
+        "description": "Parse products by url through pagination",
+    },
+]
+
+app = FastAPI(openapi_tags=tags_metadata)
+
+@app.post("/whois", tags=['whois'])
+def whois(url: str):
+    markets = ["amazon", "alibaba"]
+    domain = get_domain(url)
+
+    if domain in markets:
+        platform = domain
+    else:
+        platform = get_platform(url)
+
+    return {"domain": domain, "platform": platform}
+
+@app.post("/parse", tags=['parse'])
 def parse_by_url(url: str):
     platform = whois(url)['platform']
+    print("Parse by URL: %s" % platform)
     
     if platform in locators_mapping:
         market_locators = locators_mapping[platform]
@@ -157,6 +104,7 @@ def parse_by_url(url: str):
             data = []
 
             while next_button_disabled == False:
+                print("Wait selector %s" % market_locators["product-card"])
                 page.wait_for_selector(market_locators["product-card"])
                 next_button = page.locator(market_locators["pagination-next"])
                 data = parse_product_page(page, data, market_locators)
